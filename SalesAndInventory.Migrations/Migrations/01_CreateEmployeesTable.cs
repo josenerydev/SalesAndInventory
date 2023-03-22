2bc042c1817e7cb7a6ced3eb111f03beef8793bf
José Nery <josenerydev@gmail.com>
2023-03-22 00:39:35 -0300

    add fluent migration

---
+++ SalesAndInventory.Migrations/Migrations/01_CreateEmployeesTable.cs
@@ -0,0 +1,56 @@
+﻿using FluentMigrator;
+
+namespace SalesAndInventory.Migrations.Migrations
+{
+    [Migration(1)]
+    public class CreateEmployeesTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Schema("HR");
+
+            Create.Table("Employees")
+                .InSchema("HR")
+                .WithColumn("empid").AsInt32().PrimaryKey().Identity()
+                .WithColumn("lastname").AsString(20).NotNullable()
+                .WithColumn("firstname").AsString(10).NotNullable()
+                .WithColumn("title").AsString(30).NotNullable()
+                .WithColumn("titleofcourtesy").AsString(25).NotNullable()
+                .WithColumn("birthdate").AsDate().NotNullable()
+                .WithColumn("hiredate").AsDate().NotNullable()
+                .WithColumn("address").AsString(60).NotNullable()
+                .WithColumn("city").AsString(15).NotNullable()
+                .WithColumn("region").AsString(15).Nullable()
+                .WithColumn("postalcode").AsString(10).Nullable()
+                .WithColumn("country").AsString(15).NotNullable()
+                .WithColumn("phone").AsString(24).NotNullable()
+                .WithColumn("mgrid").AsInt32().Nullable();
+
+            Create.ForeignKey("FK_Employees_Employees")
+                .FromTable("Employees").InSchema("HR").ForeignColumn("mgrid")
+                .ToTable("Employees").InSchema("HR").PrimaryColumn("empid");
+
+            Execute.Sql("ALTER TABLE HR.Employees ADD CONSTRAINT CHK_birthdate CHECK (birthdate <= CAST(SYSDATETIME() AS DATE));");
+
+            Create.Index("idx_nc_lastname")
+                .OnTable("Employees").InSchema("HR")
+                .OnColumn("lastname").Ascending()
+                .WithOptions().NonClustered();
+
+            Create.Index("idx_nc_postalcode")
+                .OnTable("Employees").InSchema("HR")
+                .OnColumn("postalcode").Ascending()
+                .WithOptions().NonClustered();
+        }
+
+        public override void Down()
+        {
+            Delete.Index("idx_nc_postalcode").OnTable("Employees").InSchema("HR");
+            Delete.Index("idx_nc_lastname").OnTable("Employees").InSchema("HR");
+            Execute.Sql("ALTER TABLE HR.Employees DROP CONSTRAINT CHK_birthdate;");
+            Delete.ForeignKey("FK_Employees_Employees").OnTable("Employees").InSchema("HR");
+            Delete.Table("Employees").InSchema("HR");
+            Delete.Schema("HR");
+        }
+    }
+}
---
+++ SalesAndInventory.Migrations/Program.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceProvider = CreateServices();
+
+using (var scope = serviceProvider.CreateScope())
+{
+    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
+}
+
+static IServiceProvider CreateServices()
+{
+    return new ServiceCollection()
+        .AddFluentMigratorCore()
+        .ConfigureRunner(rb => rb
+            .AddSqlServer()
+            .WithGlobalConnectionString("Server=(localdb)\\mssqllocaldb;Database=test1;Trusted_Connection=True;") // Replace with your connection string
+            .ScanIn(typeof(Program).Assembly).For.Migrations())
+        .AddLogging(lb => lb.AddFluentMigratorConsole())
+        .BuildServiceProvider(false);
+}
